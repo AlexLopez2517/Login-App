d85f5d957ebae0352766c97cdd95181dfec5d18e
Alex Lopez <alex2761086444@gmail.com>
2020-11-18 10:09:20 -0600

    login y sus validaciones

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UsuarioModel } from 'src/app/models/usuario.model';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  
+  usuario: UsuarioModel = new UsuarioModel();
 
   constructor() { }
 
   ngOnInit() {
   }
+
+
+  login(form: NgForm ) {
+
+    if( form.invalid ) { return; }
+
+    console.log(this.usuario);
+    console.log('Imprimir SI el formulario es valido');
+  }
+
+
 
 }
